daec78edb64c73787f47704e3ab2a88abd13958d
ManojKumar_CT <manojkumar.s@colortokens.com>
2021-10-15 16:26:05 +0530

    cosmetic changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,13 +8,23 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
   carouselSlideData = [
-    { id: 0, bgImgClass: 'carouselSlide-0', firstText: '', secondText: '', thirdText: '' },
-    { id: 1, bgImgClass: 'carouselSlide-1', firstText: '', secondText: '', thirdText: '' },
-    { id: 2, bgImgClass: 'carouselSlide-2', firstText: '', secondText: '', thirdText: '' },
-    { id: 3, bgImgClass: 'carouselSlide-3', firstText: '', secondText: '', thirdText: '' }
+    { id: 0, bgImgClass: 'carouselSlide-0', firstText: 'First', secondText: '', thirdText: '' },
+    { id: 1, bgImgClass: 'carouselSlide-1', firstText: 'Second', secondText: '', thirdText: '' },
+    { id: 2, bgImgClass: 'carouselSlide-2', firstText: 'Third', secondText: '', thirdText: '' },
+    { id: 3, bgImgClass: 'carouselSlide-3', firstText: 'Fourth', secondText: '', thirdText: '' }
   ];
 
+  pageMenus = [
+    { name: 'Home', path: 'home' },
+    { name: 'Features', path: 'features' },
+    { name: 'About', path: 'about' },
+    { name: 'Gallery', path: 'gallery' },
+    { name: 'Friends', path: 'friends' },
+    { name: 'Testimonial', path: 'testimonial' },
+    { name: 'Contact', path: 'contact' }
+  ]
+
   getCarouselClass(index: number) {
-    return this.carouselSlideData[index].bgImgClass; 
+    return this.carouselSlideData[index].bgImgClass;
   }
 }
