5e101185f075bb15c751500ab567a991604341c0
ManojKumar_CT <manojkumar.s@colortokens.com>
2021-11-07 12:38:30 +0530

    services and contact us blocks done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,14 +15,21 @@ export class AppComponent implements OnInit, AfterViewInit {
   ];
 
   pageMenus = [
-    { name: 'Home', path: 'home' },
-    { name: 'Features', path: 'features' },
-    { name: 'About', path: 'about' },
-    { name: 'Gallery', path: 'gallery' },
-    { name: 'Friends', path: 'friends' },
-    { name: 'Testimonial', path: 'testimonial' },
-    { name: 'Contact', path: 'contact' }
+    { name: 'Home', path: 'home', block: '#homeBlock' },
+    { name: 'Services', path: 'services', block: '#servicesBlock' },
+    { name: 'About us', path: 'aboutUs', block: '#aboutUsBlock' },
+    { name: 'Contact Us', path: 'contactUs', block: '#contactUs' }
   ];
+
+  goToBlock(blockName: string) {
+    var block: HTMLElement = <HTMLElement>document.getElementById(blockName);
+    block?.scrollIntoView({ behavior: 'smooth' });
+    /* if (blockName != 'homeBlock') {
+      setTimeout(() => {
+        document.documentElement.scrollTop = document.documentElement.scrollTop + 165;
+      }, 1000);
+    } */
+  }
 
   constructor(private cdf: ChangeDetectorRef) { }
 
@@ -49,5 +56,19 @@ export class AppComponent implements OnInit, AfterViewInit {
   getCarouselClass(index: number) {
     return this.carouselSlideData[index].bgImgClass;
   }
+
+  serviceData = [
+    { title: 'Buy Property', image: 'buyProperty.png', content: 'Invest in real estate today and reap the benefits tomorrow. We have huge connectivity to let you choose and buy from.' },
+    { title: 'Rent Property', image: 'rentProperty.png', content: 'Discover homes that fit into your price range. Tell us your budget and we will find the best homes for your choice.' },
+    { title: 'Resale Property', image: 'resaleProperty.png', content: 'Sell your existing property to make the most out of it. We will direct the right choice of customers to you.' },
+    { title: 'Registration and stamps', image: 'registrationAndStamps.png', content: 'Sell your existing property to make the most out of it. We will direct the right choice of customers to you.' }
+  ];
+
+  contactData = [
+    { image: 'home.png', title: 'Our Office Address', content: 'Malad West, Mumbai, Maharashtra 400064' },
+    { image: 'email.png', title: 'General Enquiries', content: 'contact@mysites.com' },
+    { image: 'phone.png', title: 'Call Us', content: '+91-8888888888' },
+    { image: 'time.png', title: 'Our Office Address', content: 'Mon - Sun : 09:00 AM - 09:00 PM' },
+  ]
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CarouselModule } from 'ngx-bootstrap/carousel';
 import { CommonModule } from '@angular/common';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,
